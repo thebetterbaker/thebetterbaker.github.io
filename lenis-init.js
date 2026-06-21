3685c72bb173d855ecddd1f3086f61d4ec2c832c
Chaim Mendes <chaim.mendes@gmail.com>
2026-06-21 20:13:14 +0530

    lenis scroll and download fix

---
+++ lenis-init.js
@@ -0,0 +1,17 @@
+// Initialize Lenis smooth scrolling
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    direction: 'vertical',
+    gestureDirection: 'vertical',
+    smooth: true,
+    smoothTouch: false,
+    touchMultiplier: 2,
+});
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
